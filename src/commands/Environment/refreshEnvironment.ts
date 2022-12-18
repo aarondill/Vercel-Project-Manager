1d53360cdc88101c8b014238fa5682fea5689ef0
aarondill <aaronsacks2006@gmail.com>
2022-12-17 21:51:08 -0600

    Removed TODO comment

--- src/commands/Environment/refreshEnvironment.ts
+++ src/commands/Environment/refreshEnvironment.ts
@@ -5,6 +5,6 @@ export class RefreshEnvironment implements Command {
 	public readonly id = "vercel.refreshEnvironment";
 	constructor(private readonly vercel: VercelManager) {}
 	execute() {
-		this.vercel.onDidEnvironmentsUpdated(); //TODO! DEFINE THIS!!
+		this.vercel.onDidEnvironmentsUpdated();
 	}
 }
